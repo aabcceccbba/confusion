5e97168cef9143cf67cbe12b7194245e63295de5
aabcceccbba <kaiyuelu@gmail.com>
2021-12-31 17:26:15 -0600

    displayed menu in the web page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-import {Navbar, NavbarBrand} from 'reactstrap';
-
-import logo from './logo.svg';
 
 function App() {
-  return (<div className = 'App'><Navbar dark color = 'primary'>
-          <div className = 'container'><NavbarBrand href = '/'>Ristorante Con
-              Fusion</NavbarBrand>
-        </div></Navbar>
-    </div>);
+  return (
+    <div>
+      <Navbar dark color = 'primary'>
+        <div className = 'container'>
+          <NavbarBrand href = '/'>Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+    );
 }
 
 export default App;
